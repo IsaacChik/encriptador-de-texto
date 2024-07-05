8f26131e6eaba03024aef5e7f748406fc3e94f3c
IsaacChik <isaacchiquito716@gmail.com>
2024-07-05 12:02:22 -0600

    Actualización 3. Añadiendo JS con funciones de encriptar y desencriptar.

---
+++ js/main.js
@@ -0,0 +1,39 @@
+function encriptarDesencriptar(desEn) {
+    
+    const texto = document.getElementById('mensajeEscrito__textArea').value;
+    const regex = /^[a-z ñ]+$/;
+    if(regex.test(texto)){
+        if (desEn == true) {
+            var textoEncriptado = texto
+                .replace(/e/g, 'enter')
+                .replace(/i/g, 'imes')
+                .replace(/a/g, 'ai')
+                .replace(/o/g, 'ober')
+                .replace(/u/g, 'ufat');
+        }else if(desEn == false){
+            var textoEncriptado = texto
+            .replace(/enter/g, 'e')
+            .replace(/imes/g, 'i') 
+            .replace(/ai/g, 'a')
+            .replace(/ober/g, 'o')
+            .replace(/ufat/g, 'u')
+        ;
+        }
+        
+        
+
+        document.getElementById('sectionSinTexto').style.display="none";
+        document.getElementById('divMensaje').style.padding="2rem";
+        document.getElementById('sectionConTexto').style.display="flex";
+        document.getElementById('textareaMensajeEncriptado').value=textoEncriptado;
+    }else{
+        if(texto === ""){
+            document.getElementById('sectionConTexto').style.display="none";
+            document.getElementById('divMensaje').style.padding="15.1875rem 2rem";
+            document.getElementById('sectionSinTexto').style.display="flex";
+        }else{
+            alert(`No valido ${texto}`);
+        }
+        
+    }
+}
