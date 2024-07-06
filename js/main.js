694943a071af86aca04fb0d5adc72e65df5cf74e
IsaacChik <isaacchiquito716@gmail.com>
2024-07-05 22:08:39 -0600

    Actualización 5. Agregando pantallas redimensionables y una animación de aviso.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
+let avisoTxt, resizeVal1, resizeVal2;
 document.getElementById('mensajeEscrito__textArea').value="";
+verificarAnchoVentana();
+window.addEventListener('resize', verificarAnchoVentana);
+function verificarAnchoVentana() {
+    var anchoVentana = window.innerWidth;
+    
+    if(anchoVentana > 1396){
+        resizeVal1 = '2rem';
+        resizeVal2 = '15.1875rem 2rem';
+    }else if(anchoVentana <= 1396){
+        resizeVal1 = '2rem';
+        resizeVal2 = '2rem';
+
+    }
+}
+
 function encriptarDesencriptar(desEn) {
     
     const texto = document.getElementById('mensajeEscrito__textArea').value;
@@ -24,17 +40,22 @@ function encriptarDesencriptar(desEn) {
         
 
         document.getElementById('sectionSinTexto').style.display="none";
-        document.getElementById('divMensaje').style.padding="2rem";
+        document.getElementById('divMensaje').style.padding=resizeVal1;
         document.getElementById('sectionConTexto').style.display="flex";
         document.getElementById('textareaMensaje').value=textoEncriptado;
     }else{
         if(texto === ""){
-            document.getElementById('sectionConTexto').style.display="none";
-            document.getElementById('divMensaje').style.padding="15.1875rem 2rem";
-            document.getElementById('sectionSinTexto').style.display="flex";
+            avisoTxt = "No ha ingresado nada.";
         }else{
-            alert(`No valido ${texto}`);
+            avisoTxt = "Solo letras minúsculas y sin acentos";
         }
+        
+            document.getElementById('sectionConTexto').style.display="none";
+            document.getElementById('divMensaje').style.padding=resizeVal2;
+            document.getElementById('sectionSinTexto').style.display="flex";
+            
+            parpadeo();
+        
         
     }
 }
@@ -44,4 +65,13 @@ function copiar(event){
     const textoACopiar = document.getElementById('textareaMensaje');
     textoACopiar.select();
     document.execCommand('copy');
+}
+
+function parpadeo() {
+    var aviso = document.getElementById('aviso');
+    aviso.classList.add('parpadeando');
+    document.getElementById('aviso').innerHTML = `<img src='./images/exclamation.svg' alt='exclamation'></img> ${avisoTxt}`;
+    setTimeout(function() {
+        aviso.classList.remove('parpadeando');
+    }, 1800);
 }
