3eaf73e29ed683f035df2ec51400a627159f8f5a
IsaacChik <isaacchiquito716@gmail.com>
2024-07-08 22:04:47 -0600

    Actualización 6. Añadiendo animaciones.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let avisoTxt, resizeVal1, resizeVal2;
+let avisoTxt, resizeVal1, resizeVal2, time;
 document.getElementById('mensajeEscrito__textArea').value="";
 verificarAnchoVentana();
 window.addEventListener('resize', verificarAnchoVentana);
@@ -16,7 +16,9 @@ function verificarAnchoVentana() {
 }
 
 function encriptarDesencriptar(desEn) {
-    
+    if(time){
+        clearTimeout(time);
+    }
     const texto = document.getElementById('mensajeEscrito__textArea').value;
     const regex = /^[a-z ñ]+$/;
     if(regex.test(texto)){
@@ -45,9 +47,9 @@ function encriptarDesencriptar(desEn) {
         document.getElementById('textareaMensaje').value=textoEncriptado;
     }else{
         if(texto === ""){
-            avisoTxt = "No ha ingresado nada.";
+            avisoTxt = "Por favor ingrese un texto.";
         }else{
-            avisoTxt = "Solo letras minúsculas y sin acentos";
+            avisoTxt = "Evita utilizar símbolos o mayúsculas.";
         }
         
             document.getElementById('sectionConTexto').style.display="none";
@@ -56,22 +58,50 @@ function encriptarDesencriptar(desEn) {
             
             parpadeo();
         
-        
     }
 }
 
 function copiar(event){
+    if(time){
+        clearTimeout(time);
+    }
+
+    avisoTxt="Texto copiado con exito.";
     event.preventDefault();
     const textoACopiar = document.getElementById('textareaMensaje');
     textoACopiar.select();
     document.execCommand('copy');
+
+    var aviso = document.getElementById('btnCopiar');
+    aviso.classList.add('parpadeandoB');
+    let btnCopiar = document.getElementById('btnCopiar');
+    btnCopiar.innerHTML = ` ${avisoTxt} <img src='./images/verificado.png' alt='verificado'></img>`;
+    btnCopiar.style.backgroundColor="#68ff68";
+    btnCopiar.setAttribute("disabled", "true");
+    btnCopiar.style.justifyContent="space-between";
+    btnCopiar.style.border="1px solid #68ff68";
+    time = setTimeout(function() {
+        aviso.classList.remove('parpadeandoB');
+        avisoTxt = "Copiar";
+        btnCopiar.style.backgroundColor="white";
+        btnCopiar.style.color="#0A3871";
+        btnCopiar.style.justifyContent ="center";
+        btnCopiar.style.border="1px solid #0A3871";
+    btnCopiar.innerHTML = `${avisoTxt}`;
+    btnCopiar.removeAttribute("disabled");
+        time = null;
+}, 1200);
 }
 
 function parpadeo() {
     var aviso = document.getElementById('aviso');
-    aviso.classList.add('parpadeando');
+    aviso.classList.add('parpadeandoM');
     document.getElementById('aviso').innerHTML = `<img src='./images/exclamation.svg' alt='exclamation'></img> ${avisoTxt}`;
-    setTimeout(function() {
-        aviso.classList.remove('parpadeando');
-    }, 1800);
+    time = setTimeout(function() {
+        aviso.classList.remove('parpadeandoM');
+        avisoTxt = "Solo letras minúsculas y sin acentos";
+    document.getElementById('aviso').innerHTML = `<img src='./images/exclamation.svg' alt='exclamation'></img> ${avisoTxt}`;
+        time = null;
+}, 1700);
+    
 }
